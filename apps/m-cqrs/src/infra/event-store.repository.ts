5ed34893032752790e57cce5912de60216d40429
dmitryhruzin <dhr@dbbsoftware.com>
2025-12-10 01:03:17 +0200

    mCQRS move to infra

--- apps/m-cqrs/src/infra/event-store.repository.ts
+++ apps/m-cqrs/src/infra/event-store.repository.ts
@@ -4,32 +4,15 @@ import { InjectConnection } from 'nest-knexjs'
 import { InjectLogger, Logger } from '@DSAV-CQRSES-RPM/logger'
 import { Event } from '../types/common.js'
 
-/**
- * Repository for managing event store operations.
- *
- * @class EventStoreRepository
- */
 @Injectable()
 export class EventStoreRepository {
   private tableName: string = 'events'
 
-  /**
-   * Constructs an instance of EventStoreRepository.
-   *
-   * @param {Knex} knex - The knex instance for database operations.
-   * @param {Logger} logger - The logger instance.
-   */
   constructor(
-    // @ts-ignore
     @InjectConnection() private readonly knexConnection: knex.Knex,
     @InjectLogger(EventStoreRepository.name) private readonly logger: Logger
   ) {}
 
-  /**
-   * Initializes the repository and creates the events table if it doesn't exist.
-   *
-   * This method is called during the module initialization phase.
-   */
   async onModuleInit() {
     if (!(await this.knexConnection.schema.hasTable(this.tableName))) {
       await this.knexConnection.schema.createTable(this.tableName, (table) => {
@@ -44,15 +27,6 @@ export class EventStoreRepository {
     }
   }
 
-  /**
-   * Saves events to the event store.
-   *
-   * @param {string} aggregateId - The aggregate ID.
-   * @param {Event[]} events - The list of events to save.
-   * @returns {Promise<boolean>} Whether the events were successfully saved.
-   *
-   * This method inserts a list of events into the event store, associated with the specified aggregate ID.
-   */
   async saveEvents(aggregateId: string, events: Event[], trx: knex.Knex.Transaction): Promise<void> {
     if (!aggregateId) {
       throw new Error('Can not save events. Aggregate ID is not defined.')
--- apps/m-cqrs/src/infra/infra.module.ts
+++ apps/m-cqrs/src/infra/infra.module.ts
@@ -2,15 +2,11 @@ import { LoggerModule } from '@DSAV-CQRSES-RPM/logger'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { EventStoreRepository } from './event-store.repository.js'
+import { Aggregate } from './aggregate.js'
 
-/**
- * Module for managing event store related dependencies and providers.
- *
- * @module EventStoreModule
- */
 @Module({
   imports: [ConfigModule, LoggerModule],
-  providers: [EventStoreRepository],
-  exports: [EventStoreRepository]
+  providers: [Aggregate, EventStoreRepository],
+  exports: [Aggregate, EventStoreRepository]
 })
-export class EventStoreModule {}
+export class InfraModule {}
